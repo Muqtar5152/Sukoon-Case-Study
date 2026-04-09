ecaf00aff493b54935a8e9c7759da9ea7b720714
Muqtar <muqtarpasha5152@gmail.com>
2026-04-09 23:46:58 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import base44 from "@base44/vite-plugin"
+import Muqtar from "@Muqtar/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGithubActions ? '/Sukoon-Case-Study/' : '/',
   plugins: [
-    base44({
-      // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+    Muqtar({
+      // Support for legacy code that imports the Muqtar SDK with @/integrations, @/entities, etc.
+      // can be removed if the code has been updated to use the new SDK imports from @Muqtar/sdk
+      legacySDKImports: process.env.Muqtar_LEGACY_SDK_IMPORTS === 'true',
       hmrNotifier: true,
       navigationNotifier: true,
       analyticsTracker: true,
